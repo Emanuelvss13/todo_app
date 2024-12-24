cb6ca825a96d9542473490f28eb543dc1993b70b
Emanuelvss13 <emanuelsouza2002@gmail.com>
2024-12-24 17:40:18 -0300

    add react to project

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const rootElement = document.getElementById('root');
+  
+  
+  if (rootElement) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(<App />);
+  }
+});
---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, React with esbuild in Rails!</h1>
+    </div>
+  );
+}
+
+export default App;
